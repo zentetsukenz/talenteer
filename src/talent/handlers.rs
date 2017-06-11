d1bfbf16bbc705ca3ed19910f6d59af56a66ea10
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-11 12:00:43 +0700

    Move model into its own module

--- src/talent/handlers.rs
+++ src/talent/handlers.rs
@@ -6,6 +6,7 @@ extern crate bodyparser;
 
 use iron::prelude::*;
 use iron::status;
+use talent::model::*;
 
 pub fn post_talent(request: &mut Request) -> IronResult<Response> {
     match request.get::<bodyparser::Struct<TalentForm>>() {
@@ -19,18 +20,3 @@ fn create_talent(new_talent: TalentForm) -> String {
     let talent = Talent { name: new_talent.name, username: new_talent.username };
     serde_json::to_string(&talent).unwrap()
 }
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct TalentForm {
-    name: String,
-    username: String,
-    password: String,
-    password_confirmation: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Talent {
-    name: String,
-    username: String,
-}
-
--- src/talent/mod.rs
+++ src/talent/mod.rs
@@ -1,1 +1,2 @@
 pub mod handlers;
+mod model;
