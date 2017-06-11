cb5f0e7708fc4b487a263ba83c926dd091905612
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-11 22:51:58 +0700

    Add create/update timestamp and not null uuid

--- migrations/20170611113013_create_talents/up.sql
+++ migrations/20170611113013_create_talents/up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE talents (
   id              SERIAL PRIMARY KEY,
-  uid             uuid DEFAULT uuid_generate_v4(),
+  uid             uuid DEFAULT uuid_generate_v4() NOT NULL,
   username        VARCHAR NOT NULL,
   password_digest VARCHAR NOT NULL,
-  data            JSON
+  data            JSON,
+  created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated_at      TIMESTAMP WITH TIME ZONE NOT NULL
 )
