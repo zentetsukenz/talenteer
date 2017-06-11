8f84b9833a995dba759c519cf9cb2f5d36fcb6f9
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-12 00:59:27 +0700

    Use JSONB instead of JSON
    There is a performance penalty for JSONB while inserting but performance
    is improved when process. It also allows index too.

--- migrations/20170611113013_create_talents/up.sql
+++ migrations/20170611113013_create_talents/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE talents (
   id              SERIAL PRIMARY KEY,
   username        VARCHAR NOT NULL,
   password_digest VARCHAR NOT NULL,
-  data            JSON,
+  data            JSONB,
   created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
   updated_at      TIMESTAMP WITH TIME ZONE NOT NULL
 )
