f8ce416f80a68cb6850480dbf87a23684e95e0e3
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-11 22:37:48 +0700

    Add uuid to talent
    ID shouldn't be exposed to other party since it's not a good idea to let
    anyone be able to guess someone ID. So, UUID will be exposed instead.

--- migrations/20170611113013_create_talents/up.sql
+++ migrations/20170611113013_create_talents/up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE talents (
   id              SERIAL PRIMARY KEY,
+  uid             uuid DEFAULT uuid_generate_v4(),
   username        VARCHAR NOT NULL,
   password_digest VARCHAR NOT NULL,
   data            JSON
