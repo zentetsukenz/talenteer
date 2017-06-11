961dd37b0433597420cbbb52b01843bcc97ebe7f
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-11 23:42:36 +0700

    Remove UUID column from talents
    Since there should be a natural uniq field that can be used instead of
    UUID. It's also upto client which field should be used to display.

--- migrations/20170611113013_create_talents/up.sql
+++ migrations/20170611113013_create_talents/up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE talents (
   id              SERIAL PRIMARY KEY,
-  uid             uuid DEFAULT uuid_generate_v4() NOT NULL,
   username        VARCHAR NOT NULL,
   password_digest VARCHAR NOT NULL,
   data            JSON,
